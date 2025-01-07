a7e92e67beda555e95819fd1aa74a44351ae833f
MatheoBonnet <matheo.bonnet@edu.univ-fcomte.fr>
2025-01-07 10:56:46 +0100

    nzzf

--- Caca.java
+++ Caca.java
@@ -17,26 +17,27 @@ public static double corr(double[] sig1, double[] sig2, int lag) {
         return result;
     }
 
+    public class CrossCorrelationOptimized {
+        public static double[] crossCorrelation(double[] signal1, double[] signal2) {
+            int len1 = signal1.length;
+            int len2 = signal2.length;
+            int maxOffset = len1 + len2 - 1; // Nombre total de décalages possibles
+            double[] corr = new double[maxOffset];
+
+            for (int offset = 0; offset < maxOffset; offset++) {
+                double sum = 0.0;
+
+                for (int i = 0; i < len1; i++) {
+                    int j = offset - i; // Index du deuxième signal
+
+                    if (j >= 0 && j < len2) {
+                        sum += signal1[i] * signal2[j];
+                    }
+                }
+
+                corr[offset] = sum;
+            }
 
 
 
-
-
-    public static double[] crosscorrelationFFT(double[] sig1, double[] sig2) {
-        int N = sig1.length;
-        // Perform FFT
-        Complex[] fftSig1 = FFT.forward(sig1);
-        Complex[] fftSig2 = FFT.forward(sig2);
-
-        // Element-wise multiplication with conjugate
-        Complex[] product = new Complex[N];
-        for (int i = 0; i < N; i++) {
-            product[i] = fftSig1[i].multiply(fftSig2[i].conjugate());
         }
-
-        // Perform inverse FFT
-        double[] result = FFT.inverse(product);
-
-        return result;
-    }
-}
