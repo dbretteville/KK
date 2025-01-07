d2a155acf9fa6111c6c3d6696134723984c376ef
MatheoBonnet <matheo.bonnet@edu.univ-fcomte.fr>
2025-01-07 11:29:32 +0100

    efnndr

--- Caca.java
+++ Caca.java
@@ -1,43 +1,76 @@
 public class Caca {
-public static double corr(double[] sig1, double[] sig2, int lag) {
-        double sum = 0;
-        for (int i = 0; i < sig1.length; i++) {
-            int shiftedIndex = (i + lag) % sig2.length;
-            sum += sig1[i] * sig2[shiftedIndex];
-        }
-        return sum;
-    }
 
-    public static double[] crosscorrelationNaive(double[] sig1, double[] sig2) {
-        int N = sig1.length;
-        double[] result = new double[N];
-        for (int lag = 0; lag < N; lag++) {
-            result[lag] = corr(sig1, sig2, lag);
-        }
-        return result;
-    }
+    public static double[] crossCorrelationsimple(double[] signal1, double[] signal2) {
+        int len1 = signal1.length;
+        int len2 = signal2.length;
+        int maxOffset = len1 + len2 - 1; // Nombre total de décalages possibles
+        double[] corr = new double[maxOffset];
 
-    public class CrossCorrelationOptimized {
-        public static double[] crossCorrelation(double[] signal1, double[] signal2) {
-            int len1 = signal1.length;
-            int len2 = signal2.length;
-            int maxOffset = len1 + len2 - 1; // Nombre total de décalages possibles
-            double[] corr = new double[maxOffset];
+        // Calcul de la corrélation pour chaque décalage
+        for (int offset = 0; offset < maxOffset; offset++) {
+            double sum = 0.0;
 
-            for (int offset = 0; offset < maxOffset; offset++) {
-                double sum = 0.0;
+            for (int i = 0; i < len1; i++) {
+                int j = offset - i; // Index du second signal
 
-                for (int i = 0; i < len1; i++) {
-                    int j = offset - i; // Index du deuxième signal
-
-                    if (j >= 0 && j < len2) {
-                        sum += signal1[i] * signal2[j];
-                    }
+                // Vérifier si l'indice j est dans les limites de signal2
+                if (j >= 0 && j < len2) {
+                    sum += signal1[i] * signal2[j];
                 }
+            }
+            corr[offset] = sum; // Stocker la valeur de corrélation pour ce décalage
+        }
+        System.out.println("testzfo");
+        return corr;
+    }
 
-                corr[offset] = sum;
+    public static double[] crossCorrelation(double[] signal1, double[] signal2) {
+        int len1 = signal1.length;
+        int len2 = signal2.length;
+        int maxOffset = len1 + len2 - 1; // Nombre total de décalages possibles
+        double[] corr = new double[maxOffset];
+
+        for (int offset = 0; offset < maxOffset; offset++) {
+            double sum = 0.0;
+
+            for (int i = 0; i < len1; i++) {
+                int j = offset - i; // Index du deuxième signal
+
+                if (j >= 0 && j < len2) {
+                    sum += signal1[i] * signal2[j];
+                }
             }
 
-
-
+            corr[offset] = sum;
         }
+        return corr;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("hello");
+        String wav1 = args[0];
+        String wav2 = args[1];
+        double[] inputWav1 = StdAudio.read(wav1);
+        double[] inputWav2 = StdAudio.read(wav2);
+        System.out.println("zknzvevjne");
+        double[] res1 = crossCorrelationsimple(inputWav1,inputWav2);
+        //double[] res2 = crossCorrelation(inputWav1,inputWav2);
+
+        System.out.println("test");
+
+        System.out.println("cross corelation naive");
+        for (int i = 0; i < res1.length; i++) {
+            System.out.println(res1[i]);
+        }
+/*
+        System.out.println("cross corelation optimise");
+        for (int i = 0; i < res2.length; i++) {
+            System.out.println(res2[i]);
+        }*/
+    }
+
+}
+
+
+
+
